cdd2e194fd710e5841c1924b832155720874c104
Saulo Emídio <sauloemidio@gmail.com>
2023-02-11 19:40:18 -0300

    implementando lógica

---
+++ script.js
@@ -0,0 +1,69 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+const btnEncriptar = document.querySelector(".btn-encriptar");
+const btnDesincriptar = document.querySelector(".btn-desencriptar");
+
+let indice = 0;
+let indiceCripto = 1;
+
+
+btnEncriptar.addEventListener('click', ()=>{
+    encryptDecrypt();
+})
+
+btnDesincriptar.addEventListener('click', ()=>{
+    encryptDecrypt();
+})
+
+function encryptDecrypt(){
+    if(indice === 1){
+        let fraseEncriptada = matrixCrypto(mensagem.value);        
+        textArea.value = fraseEncriptada;
+        console.log(fraseEncriptada);
+        mensagem.value = "";
+        retirarBackground();
+    } else{
+        let fraseEncriptada = matrixCrypto(textArea.value);
+        mensagem.value = fraseEncriptada;
+        textArea.value = "";
+        retirarBackground();
+
+    }
+    
+        
+}
+
+    function retirarBackground(){
+    if(mensagem.value.length > 0){
+        mensagem.style.backgroundImage = "url('')";
+    }else{
+        mensagem.style.backgroundImage = "url('/imagens/boneco.png')";
+    }
+    
+    }
+
+
+function matrixCrypto(frase){
+
+    let matrixCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    frase = frase.toLowerCase();
+
+
+
+    for(let i = 0; i < matrixCode.length; i ++){
+        if(frase.includes(matrixCode[i][indice])){
+            frase = frase.replaceAll(matrixCode[i][indice], matrixCode[i][indiceCripto])
+        }
+    }
+    if(indice === 0){
+        indice = 1;
+        indiceCripto = 0;
+    } else{
+        indice = 0;
+        indiceCripto = 1;
+    }
+        
+        return frase;
+
+    
+}
